ae0c31942f5796933ebe41717288152011517796
calvin-fisher <calvin.fisher@gmail.com>
2016-02-23 15:01:48 -0600

    Added page model logic

--- FreshMvvmDemo.UI/PageModels/StoreDetailsPageModel.cs
+++ FreshMvvmDemo.UI/PageModels/StoreDetailsPageModel.cs
@@ -1,8 +1,57 @@
-﻿using FreshMvvm;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FreshMvvm;
+using FreshMvvmDemo.Common;
+using FreshMvvmDemo.Common.Models;
 
 namespace FreshMvvmDemo.UI.PageModels
 {
     public class StoreDetailsPageModel : FreshBasePageModel
     {
+        public StoreDetailsPageModel()
+        {
+            _storeDataService = FreshIOC.Container.Resolve<IStoreDataService>();
+        }
+
+        private readonly IStoreDataService _storeDataService;
+
+        public override void Init(object initData)
+        {
+            var store = initData as Store;
+            if (store == null)
+                throw new ArgumentException("Could not load store details page with null init data.");
+
+            Store = store;
+
+            LoadProducts();
+
+            base.Init(initData);
+        }
+
+        public Store Store { get; private set; }
+
+        private readonly SemaphoreSlim _productsLoadingLock = new SemaphoreSlim(1);
+        public bool ProductsLoading { get; private set; }
+        public Product[] Products { get; private set; }
+
+        private async void LoadProducts()
+        {
+            if (!_productsLoadingLock.Wait(0))
+                return;
+
+            try
+            {
+                ProductsLoading = true;
+                
+                var storeProducts = await _storeDataService.GetProducts(Store.Id);
+                Products = storeProducts;
+            }
+            finally
+            {
+                ProductsLoading = false;
+                _productsLoadingLock.Release();
+            }
+        }
     }
 }
--- FreshMvvmDemo.UI/PageModels/StoreSelectionPageModel.cs
+++ FreshMvvmDemo.UI/PageModels/StoreSelectionPageModel.cs
@@ -1,8 +1,49 @@
-﻿using FreshMvvm;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FreshMvvm;
+using FreshMvvmDemo.Common;
+using FreshMvvmDemo.Common.Models;
 
 namespace FreshMvvmDemo.UI.PageModels
 {
     public class StoreSelectionPageModel : FreshBasePageModel
     {
+        public StoreSelectionPageModel()
+        {
+            _storeDataService = FreshIOC.Container.Resolve<IStoreDataService>();
+        }
+
+        private readonly IStoreDataService _storeDataService;
+
+        public override void Init(object initData)
+        {
+            LoadStores();
+
+            base.Init(initData);
+        }
+
+        private readonly SemaphoreSlim _storesLoadingLock = new SemaphoreSlim(1);
+        public bool StoresLoading { get; private set; }
+        public Store[] Stores { get; private set; }
+
+        private async void LoadStores()
+        {
+            if (!_storesLoadingLock.Wait(0))
+                return;
+
+            try
+            {
+                StoresLoading = true;
+
+                var stores = await _storeDataService.GetStores();
+                Stores = stores;
+            }
+            finally
+            {
+                StoresLoading = false;
+                _storesLoadingLock.Release();
+            }
+        }
     }
 }

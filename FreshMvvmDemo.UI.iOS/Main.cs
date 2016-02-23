28a351fff874c6c405d418667f002db425ef2e7e
calvin-fisher <calvin.fisher@gmail.com>
2016-02-22 18:29:29 -0600

    Added Forms init & bindings

--- FreshMvvmDemo.UI.iOS/Main.cs
+++ FreshMvvmDemo.UI.iOS/Main.cs
@@ -1,19 +1,11 @@
-﻿using FreshMvvm;
-using FreshMvvmDemo.Common;
-using FreshMvvmDemo.DummyServiceClient;
-using UIKit;
+﻿using UIKit;
 
 namespace FreshMvvmDemo.UI.iOS
 {
     public class Application
     {
-        // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            FreshIOC.Container.Register<IStoreDataService, DummyStoreDataService>().AsSingleton();
-
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
---
+++ FreshMvvmDemo.UI/App.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreshMvvm;
+using FreshMvvmDemo.Common;
+using FreshMvvmDemo.DummyServiceClient;
+using FreshMvvmDemo.UI.PageModels;
+using Xamarin.Forms;
+
+namespace FreshMvvmDemo.UI
+{
+    public class App : Application
+    {
+        public App()
+        {
+            Bootstrap();
+
+            var page = FreshPageModelResolver.ResolvePageModel<StoreSelectionPageModel>();
+            var navigation = new FreshNavigationContainer(page);
+            MainPage = navigation;
+        }
+
+        private static void Bootstrap()
+        {
+            FreshIOC.Container.Register<IStoreDataService, DummyStoreDataService>().AsSingleton();
+        }
+    }
+}
